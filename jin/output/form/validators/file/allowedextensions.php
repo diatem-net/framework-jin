f371981da5dd08b2f7e709ca3865c82ec800a290
Samuel Marchal <smarchal@diatem.net>
2014-11-03 16:14:09 +0100

    Bug fix: empty file hasn't any extension

--- jin/output/form/validators/file/allowedextensions.php
+++ jin/output/form/validators/file/allowedextensions.php
@@ -14,9 +14,9 @@ use jin\lang\StringTools;
 
 /** Validateur : teste si le fichier est de types précisés
  *
- * 	@auteur		Loïc Gerard
- * 	@version	0.0.1
- * 	@check		
+ *  @auteur     Loïc Gerard
+ *  @version    0.0.1
+ *  @check
  */
 class Allowedextensions extends GlobalFileValidator implements ValidatorInterface{
     /**
@@ -24,34 +24,34 @@ class Allowedextensions extends GlobalFileValidator implements ValidatorInterfac
      * @param type $args    Tableau d'arguments. extensionList (Liste d'extensions supportées. ex: jpg,csv)
      */
     public function __construct($args) {
-	parent::__construct($args, array('extensionList'));
+        parent::__construct($args, array('extensionList'));
     }
-    
+
     /**
      * Teste la validité
      * @param array $valeur Valeur $_FILES à tester
      * @return boolean
      */
     public function isValid($valeur){
-	parent::resetErrors();
-	
-	$currentExt = ListTools::last($valeur['name'], '.');
-	if(!ListTools::containsNoCase(parent::getArgValue('extensionList'), $currentExt)){
-	    $eMsg = Trad::trad('allowedextensions');
-	    parent::addError(StringTools::replaceAll($eMsg, '%extensionList%', parent::getArgValue('extensionList')));
-	    return false;
-	}
-	
-	return true;
+        parent::resetErrors();
+
+        $currentExt = ListTools::last($valeur['name'], '.');
+        if($currentExt && !ListTools::containsNoCase(parent::getArgValue('extensionList'), $currentExt)){
+            $eMsg = Trad::trad('allowedextensions');
+            parent::addError(StringTools::replaceAll($eMsg, '%extensionList%', parent::getArgValue('extensionList')));
+            return false;
+        }
+
+        return true;
     }
-    
-    
+
+
     /**
      * Priorité NIV1 du validateur
      * @return boolean
      */
     public function isPrior(){
-	return false;
+        return false;
     }
 }
 
