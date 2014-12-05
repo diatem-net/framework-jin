4da6422cfe806cb1be0ad42e294a0d7df50aa149
Samuel Marchal <smarchal@diatem.net>
2014-12-05 16:27:25 +0100

    Add length() method to SimpleFacet

--- jin/external/diatem/sherlock/facets/simplefacet.php
+++ jin/external/diatem/sherlock/facets/simplefacet.php
@@ -13,120 +13,124 @@ class SimpleFacet implements Iterator{
     private $ESData = array();
     private $selectedValue = null;
     private $childFacets = null;
-    
+
     public function __construct($fieldName, $facetName) {
-	$this->fieldName = $fieldName;
-	$this->facetName = $facetName;
+        $this->fieldName = $fieldName;
+        $this->facetName = $facetName;
     }
-    
+
     public function getName(){
-	return $this->facetName;
+        return $this->facetName;
     }
-    
+
     public function setChildFacet(SimpleFacet $facetObject){
-	$this->childFacets = $facetObject;
+        $this->childFacets = $facetObject;
     }
-    
+
     public function getArgArrayForAggregate(){
-	$outArray = array();
-	$outArray[$this->facetName] = array();
-	$outArray[$this->facetName]['terms'] = array();
-	$outArray[$this->facetName]['terms']['field'] = $this->fieldName;
-	
-	return $outArray;
+        $outArray = array();
+        $outArray[$this->facetName] = array();
+        $outArray[$this->facetName]['terms'] = array();
+        $outArray[$this->facetName]['terms']['field'] = $this->fieldName;
+
+        return $outArray;
     }
-    
+
     public function getArgArrayForSearchQuery(){
-	if($this->selectedValue){
-	    if(is_array($this->selectedValue)){
-		$fullCond = array();
-		foreach($this->selectedValue as $value){
-		    $condition = new ConditionOnSingleTerm(array($this->fieldName), $value);
-		    $fullCond = ArrayTools::merge($fullCond, $condition->getParamArray()); 
-		}
-		return $fullCond;
-	    }else{
-		$condition = new ConditionOnSingleTerm(array($this->fieldName), $this->selectedValue);
-		return $condition->getParamArray();
-	    }
-	    
-	}
-	return null;
+        if($this->selectedValue){
+            if(is_array($this->selectedValue)){
+                $fullCond = array();
+                foreach($this->selectedValue as $value){
+                    $condition = new ConditionOnSingleTerm(array($this->fieldName), $value);
+                    $fullCond = ArrayTools::merge($fullCond, $condition->getParamArray());
+                }
+                return $fullCond;
+            }else{
+                $condition = new ConditionOnSingleTerm(array($this->fieldName), $this->selectedValue);
+                return $condition->getParamArray();
+            }
+
+        }
+        return null;
     }
-    
+
     public function setESReturnData($data){
-	if(isset($data['buckets'])){
-	    $this->ESData = $data['buckets'];
-	    foreach ($this->ESData as $key => $d){
-		$this->ESData[$key]['selected'] = $this->isValueSelected($d['key']);
-	    }
-	}
+        if(isset($data['buckets'])){
+            $this->ESData = $data['buckets'];
+            foreach ($this->ESData as $key => $d){
+                $this->ESData[$key]['selected'] = $this->isValueSelected($d['key']);
+            }
+        }
     }
-    
+
     public function isValueSelected($value){
-	if(is_array($this->selectedValue)){
-	    if(is_numeric(ArrayTools::find($this->selectedValue, $value))){
-		return true;
-	    }
-	}else{
-	    if($this->selectedValue == $value){
-		return true;
-	    }
-	}
-	return false;
+        if(is_array($this->selectedValue)){
+            if(is_numeric(ArrayTools::find($this->selectedValue, $value))){
+                return true;
+            }
+        }else{
+            if($this->selectedValue == $value){
+                return true;
+            }
+        }
+        return false;
     }
-    
+
     public function setSelectedValue($value){
-	$this->selectedValue = $value;
+        $this->selectedValue = $value;
     }
-    
+
     public function setSelectedValues($arrayOfValues){
-	$this->selectedValue = $arrayOfValues;
+        $this->selectedValue = $arrayOfValues;
     }
-    
+
+    public function length() {
+        return iterator_count($this);
+    }
+
     //Fonctions d'itération
-    
+
     /**
      * Itération : current
      * @return mixed
      */
     public function current() {
-	return current($this->ESData);
+        return current($this->ESData);
     }
 
-    
+
     /**
      * Itération : key
      * @return string
      */
     public function key() {
-	return key($this->ESData);
+        return key($this->ESData);
     }
 
-    
+
     /**
      * Itération : rewind
      * @return \jin\query\QueryResult
      */
     public function rewind() {
-	reset($this->ESData);
-	return $this;
+        reset($this->ESData);
+        return $this;
     }
 
-    
+
     /**
      * Itération : next
      */
     public function next() {
-	next($this->ESData);
+        next($this->ESData);
     }
 
-    
+
     /**
      * Itération valid
      * @return boolean
      */
     public function valid() {
-	return array_key_exists(key($this->ESData), $this->ESData);
+        return array_key_exists(key($this->ESData), $this->ESData);
     }
 }
