d41bbe98e76710e5aedb0e644555a4e83a94633e
Loïc Gerard <lgerard@diatem.net>
2015-02-06 10:38:49 +0100

    Gestion des CSV sans headers

--- jin/dataformat/csv.php
+++ jin/dataformat/csv.php
@@ -1,8 +1,10 @@
 <?php
+
 /**
  * Jin Framework
  * Diatem
  */
+
 namespace jin\dataformat;
 
 use jin\query\QueryResult;
@@ -13,18 +15,26 @@ use jin\query\QueryResult;
  * 	@check
  */
 class Csv {
+
     /**
-     *	Données lues/à écrire
+     * 	Données lues/à écrire
      * @var array  
      */
     private $data;
 
     
     /**
+     * Définit si on écrit les en-têtes de colonne ou non.
+     * @var boolean
+     */
+    private $useHeaders = true;
+
+    
+    /**
      * Constructeur
      */
     public function __construct() {
-	
+        
     }
 
     
@@ -32,21 +42,23 @@ class Csv {
      * Définit les données à écrire à partir d'un tableau de tableaux associatifs.
      * Ex. array(array('id'=>1,'col'=>'valeur1'),array('id'=>2,'col'=>'valeur2'))
      * @param array $associativeArray
+     * @param boolean $useHeaders Affiche les en-tête de colonne ou non
      */
-    public function populateWithArray($associativeArray) {
-	$this->data = $associativeArray;
+    public function populateWithArray($associativeArray, $useHeaders = true) {
+        $this->data = $associativeArray;
+        $this->useHeaders = $useHeaders;
     }
-    
-    
+
     /**
      * Définit les données à écrire à partir d'un objet QueryResult
      * @param \jin\query\QueryResult $queryResult
+     * @param boolean $useHeaders Affiche les en-tête de colonne ou non
      */
-    public function populateWithQueryResult(QueryResult $queryResult){
-	$this->data = $queryResult->getDatasInArray(true, true);
+    public function populateWithQueryResult(QueryResult $queryResult, $useHeaders = true) {
+        $this->data = $queryResult->getDatasInArray(true, true);
+        $this->useHeaders = $useHeaders;
     }
 
-    
     /**
      * Ecrit le flux CSV dans un fichier
      * @param string $filePath	Chemin relatif/absolu du fichier
@@ -54,99 +66,105 @@ class Csv {
      * @throws Exception
      */
     public function outputInFile($filePath) {
-	if (!$this->data) {
-	    throw new \Exception('Aucune donnée CSV à exporter.');
-	}
+        if (!$this->data) {
+            throw new \Exception('Aucune donnée CSV à exporter.');
+        }
 
-	// On cherche des infos sur le fichier à ouvrir
-	if (file_exists($filePath)){
-	    $infos_fichier = stat($filePath);
-	}
+        // On cherche des infos sur le fichier à ouvrir
+        if (file_exists($filePath)) {
+            $infos_fichier = stat($filePath);
+        }
 
-	// Si le fichier est inexistant ou vide, on va le créer et y ajouter les 
-	// libellés de colonne.
-	if (!file_exists($filePath) || (isset($infos_fichier) && $infos_fichier['size'] == 0)) {
+        // Si le fichier est inexistant ou vide, on va le créer et y ajouter les 
+        // libellés de colonne.
+        if (!file_exists($filePath) || (isset($infos_fichier) && $infos_fichier['size'] == 0)) {
 
-	    // On ouvre le fichier en écriture seule et on le vide de son contenu
-	    $fp = @fopen($filePath, 'w');
-	    if ($fp === false)
-		throw new Exception("Le fichier ${$filePath} n'a pas pu être créé.");
+            // On ouvre le fichier en écriture seule et on le vide de son contenu
+            $fp = @fopen($filePath, 'w');
+            if ($fp === false)
+                throw new Exception("Le fichier ${$filePath} n'a pas pu être créé.");
 
-	    // Les entêtes sont les clés du tableau associatif
-	    $entetes = array_keys($this->data[0]);
+            if($this->useHeaders){
+                // Les entêtes sont les clés du tableau associatif
+                $entetes = array_keys($this->data[0]);
 
-	    // Décodage des entêtes qui sont en UTF8 à la base
-	    foreach ($entetes as &$entete) {
-		// Notez l'utilisation de iconv pour changer l'encodage.
-		$entete = (is_string($entete)) ?
-			iconv("UTF-8", "Windows-1252//TRANSLIT", $entete) : $entete;
-	    }
+                // Décodage des entêtes qui sont en UTF8 à la base
+                foreach ($entetes as &$entete) {
+                    // Notez l'utilisation de iconv pour changer l'encodage.
+                    $entete = (is_string($entete)) ?
+                            iconv("UTF-8", "Windows-1252//TRANSLIT", $entete) : $entete;
+                }
+                
+                // On utilise le troisième paramètre de fputcsv pour changer le séparateur 
+                // par défaut de php.
+                fputcsv($fp, $entetes, ';');
+            }
 
-	    // On utilise le troisième paramètre de fputcsv pour changer le séparateur 
-	    // par défaut de php.
-	    fputcsv($fp, $entetes, ';');
-	}
+            
+        }
 
-	// On ouvre le handler en écriture pour écrire le fichier
-	// s'il ne l'est pas déjà.
-	if (!isset($fp)) {
-	    $fp = fopen($filePath, 'a');
-	}
+        // On ouvre le handler en écriture pour écrire le fichier
+        // s'il ne l'est pas déjà.
+        if (!isset($fp)) {
+            $fp = fopen($filePath, 'a');
+        }
 
-	$this->writeDataInIO($fp);
-	
+        $this->writeDataInIO($fp);
 
-	fclose($fp);
+
+        fclose($fp);
     }
 
-    
     /**
      * Ecrit le flux CSV dans la sortie navigateur
      * @param string $fileName	Nom du fichier généré
      * @throws \Exception
      */
     public function output($fileName) {
-	if (!$this->data) {
-	    throw new \Exception('Aucune donnée CSV à exporter.');
-	}
+        if (!$this->data) {
+            throw new \Exception('Aucune donnée CSV à exporter.');
+        }
 
-	header('Content-Type: application/excel');
-	header('Content-Disposition: attachment; filename="'.$fileName.'"');
+        header('Content-Type: application/excel');
+        header('Content-Disposition: attachment; filename="' . $fileName . '"');
 
-	$fp = fopen('php://output', 'w');
-	
-	// Les entêtes sont les clés du tableau associatif
-	$entetes = array_keys($this->data[0]);
+        $fp = fopen('php://output', 'w');
 
-	// Décodage des entêtes qui sont en UTF8 à la base
-	foreach ($entetes as &$entete) {
-	    // Notez l'utilisation de iconv pour changer l'encodage.
-	    $entete = (is_string($entete)) ? iconv("UTF-8", "Windows-1252//TRANSLIT", $entete) : $entete;
-	}
+        
+        if($this->useHeaders){
+            // Les entêtes sont les clés du tableau associatif
+            $entetes = array_keys($this->data[0]);
 
-	// On utilise le troisième paramètre de fputcsv pour changer le séparateur 
-	// par défaut de php.
-	fputcsv($fp, $entetes, ';');
-	// Écriture des données
-	$this->writeDataInIO($fp);
-	
-	fclose($fp);
+            // Décodage des entêtes qui sont en UTF8 à la base
+            foreach ($entetes as &$entete) {
+                // Notez l'utilisation de iconv pour changer l'encodage.
+                $entete = (is_string($entete)) ? iconv("UTF-8", "Windows-1252//TRANSLIT", $entete) : $entete;
+            }
+
+            // On utilise le troisième paramètre de fputcsv pour changer le séparateur 
+            // par défaut de php.
+            fputcsv($fp, $entetes, ';');
+        }
+        
+        // Écriture des données
+        $this->writeDataInIO($fp);
+
+        fclose($fp);
     }
-    
-    
+
     /**
      * Ecrit dans le flux de sortie
      * @param ressource $fp
      */
-    private function writeDataInIO($fp){
-	// Écriture des données
-	foreach ($this->data as $donnee) {
-	    foreach ($donnee as &$champ) {
-		$champ = (is_string($champ)) ?
-			iconv("UTF-8", "Windows-1252//TRANSLIT", $champ) : $champ;
-	    }
-	    fputcsv($fp, $donnee, ';');
-	}
+    private function writeDataInIO($fp) {
+        // Écriture des données
+        foreach ($this->data as $donnee) {
+            foreach ($donnee as &$champ) {
+                $champ = (is_string($champ)) ?
+                        iconv("UTF-8", "Windows-1252//TRANSLIT", $champ) : $champ;
+            }
+            fputcsv($fp, $donnee, ';');
+        }
     }
 
 }
