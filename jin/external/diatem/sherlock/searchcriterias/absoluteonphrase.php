f57614687753598384e747294914c66fd05c6810
Samuel Marchal <smarchal@diatem.net>
2015-05-06 18:21:07 +0200

    Improve Sherlock, allow nested Bool Queries

---
+++ jin/external/diatem/sherlock/searchcriterias/absoluteonphrase.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Jin Framework
+ * Diatem
+ */
+
+namespace jin\external\diatem\sherlock\searchcriterias;
+
+use jin\external\diatem\sherlock\SearchItemInterface;
+
+/** Filtre Sherlock de type critère : recherche absolue sur plusieurs termes
+ *
+ *  @auteur     Samuel Marchal
+ *  @version    0.0.1
+ *  @check
+ */
+class AbsoluteOnPhrase implements SearchItemInterface{
+    /**
+     *
+     * @var array Noms des champs sur lesquels appliquer le filtre
+     */
+    private $fields;
+
+    /**
+     *
+     * @var string  Valeur de test
+     */
+    public $values;
+
+
+    /** Constructeur
+     *
+     * @param array $fields Noms des champs sur lesquels appliquer le filtre
+     * @param string $values  Valeur de test
+     */
+    public function __construct($fields, $values) {
+        $this->fields = $fields;
+        $this->values = $values;
+    }
+
+
+    /** Construit le tableau destiné à être ajouté dans une requête de recherche par SherlockSearch
+     *
+     * @return array    Paramètres de recherche SherlockSearch
+     */
+    public function getParamArray(){
+
+        $critArray = array();
+        $critArray['multi_match'] = array();
+        $critArray['multi_match']['type'] = 'phrase';
+        $critArray['multi_match']['query'] = $this->values;
+        $critArray['multi_match']['fields'] = $this->fields;
+
+        return array($critArray);
+
+
+    }
+}
