aed77914e62b723e75fa45df03453efc201695eb
Loic Gerard <lgerard@diatem.net>
2015-05-26 08:32:02 +0200

    Ajout dans la classe Curl de la gestion de l'authentification HTTP, l'ajout de headers personnalises, le Content-type, parametres de debug

--- jin/com/curl.php
+++ jin/com/curl.php
@@ -10,23 +10,21 @@ namespace jin\com;
 use jin\log\Debug;
 use jin\lang\StringTools;
 
-/** Classe permettant de gÃ©rer des appels CURL
+/** Classe permettant de générer des appels CURL
  *
- * 	@auteur		LoÃ¯c Gerard
- * 	@version	0.0.1
- * 	@check
+ * 	@auteur		Loïc Gerard
  */
 class Curl {
 
     /**
      *
-     * @var string  DerniÃ¨re erreur rencontrÃ©e
+     * @var string  Dernière erreur rencontrée
      */
     private static $lastErrorText = '';
     
     /**
      *
-     * @var int	Dernier code d'erreur rencontrÃ©
+     * @var int	Dernier code d'erreur rencontré
      */
     private static $lastErrorCode = 0;
 
@@ -35,25 +33,59 @@ class Curl {
     
     /**
      * Appelle une Url
-     * @param type $url		Url Ã  appeler
-     * @param type $args	[optional] Arguments Ã  transmettre en POST (NULL par dÃ©faut)
-     * @param type $requestType	[optionel] Type de requÃªte. (POST,GET, DELETE ou PUT) (POST par dÃ©faut)
-     * @param type $throwError	[optionel] GÃ©nÃ¨re les erreurs directement (True par dÃ©faut)
+     * @param string        $url                Url à  appeler
+     * @param array|string  $args               [optional] Arguments à  transmettre (NULL par défaut)
+     * @param string        $requestType        [optionel] Type de requête. (POST,GET, DELETE ou PUT) (POST par défaut)
+     * @param boolean       $throwError         [optionel] Génère les erreurs directement (True par défaut)
+     * @param string        $contentType        [optionel] Header 'Content-type'. Par défaut : application/json
+     * @param array         $headers            [optionel] Headers additionnels. (Sous la forme d'un tableau clé/valeur)
+     * @param string        $outputTraceFile    [optionel] Si renseigné : effectue une trace des flux réseaux dans le fichier ainsi déterminé. (Chemin absolu du fichier)
      * @return boolean
      * @throws \Exception
      */
-    public static function call($url, $args = null, $requestType = 'POST', $throwError = true, $httpAuthUser = null, $httpAuthPassword = null){
+    public static function call($url, 
+            $args = null, 
+            $requestType = 'POST', 
+            $throwError = true, 
+            $httpAuthUser = null, 
+            $httpAuthPassword = null,
+            $contentType = null,
+            $headers = array(),
+            $outputTraceFile = null){
+        
 	$curl = curl_init();
 	curl_setopt($curl, CURLOPT_URL, $url);
 	curl_setopt($curl, CURLOPT_COOKIESESSION, true);
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         
+        //Set headers
+        $h = array();
+        if($contentType){
+            $h[] = 'Content-type: '.$contentType;
+        }
+        if(!empty($headers)){
+            foreach($headers AS $k => $v){
+                $h[] = $k.': '.$v;
+            }
+        }
+        if(!empty($h)){
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $h);
+        }
+        
+        //Mode debug
+        if(!empty($outputTraceFile)){
+            curl_setopt($curl, CURLOPT_VERBOSE, true);
+            $verbose = fopen($outputTraceFile, 'w');
+            curl_setopt($curl, CURLOPT_STDERR, $verbose);
+        }
+        
+        //Authentification HTTP
         if(!is_null($httpAuthUser) && !is_null($httpAuthPassword)){
             curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
             curl_setopt($curl, CURLOPT_USERPWD, $httpAuthUser.':'.$httpAuthPassword);
         }
 	
-	
+	//Delete
 	if ($requestType == 'DELETE') {
 	    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'DELETE');
 	    if(!StringTools::contains($url, '?')){
@@ -64,6 +96,7 @@ class Curl {
 	    curl_setopt($curl, CURLOPT_URL, $url);
 	}
 
+        //Get
 	if ($requestType == 'GET') {
 	    if(!StringTools::contains($url, '?')){
 		$url .= '?' . http_build_query($args);
@@ -73,18 +106,21 @@ class Curl {
 	    curl_setopt($curl, CURLOPT_URL, $url);
 	}
 
+        //Post
 	if($requestType == 'POST') {
 	    curl_setopt($curl, CURLOPT_POST, TRUE);
 	    curl_setopt($curl, CURLOPT_POSTFIELDS, $args);
 	}
 
+        //Put
 	if ($requestType == 'PUT') {
 	    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PUT');
 	    curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($args));
+            
 	}
 
-
 	$return = curl_exec($curl);
+        
 	self::$lastCurlInfo = curl_getinfo($curl);
 	
 	if (!$return) {
@@ -115,14 +151,21 @@ class Curl {
 
     
     /**
-     * Retourne la derniÃ¨re erreur rencontrÃ©e (verbose)
+     * Retourne la dernière erreur rencontrée (verbose)
      * @return string
      */
     public static function getLastErrorVerbose() {
 	return self::$lastErrorText;
     }
     
+    
+    /**
+     * Retourne le code HTTP de retour du dernier appel
+     * @return string
+     */
     public static function getLastHttpCode(){
+        Debug::dump(self::$lastCurlInfo);
+        
 	if(isset(self::$lastCurlInfo['http_code'])){
 	    return self::$lastCurlInfo['http_code'];
 	}else{
@@ -131,6 +174,10 @@ class Curl {
     }
     
     
+    /**
+     * Retourne le détail textuel du dernier code HTTP retourné
+     * @return string
+     */
     public static function getLastHttpCodeVerbose(){
 	if(isset(self::$lastCurlInfo['http_code'])){
 	    $status = array(
