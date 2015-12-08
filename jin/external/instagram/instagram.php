4e6238dd6416ad00ee9b237e804d14aeb5c76198
Samuel Marchal <smarchal@diatem.net>
2015-12-08 11:07:09 +0100

    Improve Instagram API class [BACKWARD COMPATIBILIY ISSUE]

--- jin/external/instagram/instagram.php
+++ jin/external/instagram/instagram.php
@@ -7,49 +7,86 @@
 namespace jin\external\instagram;
 
 use jin\com\Curl;
-use jin\lang\StringTools;
-use jin\log\Debug;
 use jin\dataformat\Json;
 
 /**
  * Méthodes d'implémentation de l'API Instagram.
  * https://instagram.com/developer/
  */
-class Instagram{
+class Instagram
+{
+
     /**
-     * Id client
-     * @var string
+     * @var string  Instagram CLIENT_ID
      */
-    private $clientId;
-    
-    
+    private $client_id;
+
+    /**
+     * @var string  Instagram ACCESS_TOKEN
+     * Pour en obtenir un : https://www.instagram.com/oauth/authorize/?client_id=CLIENT_ID&redirect_uri=REDIRECT_URI&response_type=token&scope=public_content
+     */
+    private $access_token;
+
+    /**
+     *
+     * @var boolean Debug mode
+     */
+    private $debug_mode;
+
     /**
      * url de l'API Instagram
      */
     const INSTAGRAM_API_URL = 'https://api.instagram.com/v1/';
-    
-    
+
     /**
      * Constructeur
-     * @param string $clientId  Id Client. (A générer sur https://instagram.com/developer/)
+     * @param string $client_id  Id Client. (A générer sur https://instagram.com/developer/)
      */
-    public function __construct($clientId) {
-        $this->clientId = $clientId;
+    public function __construct($client_id, $access_token, $debug_mode = false) {
+        $this->client_id = $client_id;
+        $this->access_token = $access_token;
+        $this->debug_mode = $debug_mode;
     }
-    
-    
+
+    /**
+     * Effectue une requête directe sur l'API
+     * @param string $query         Requête
+     * @param array  $params        [optionel] Paramètres
+     * @return Array
+     */
+    public function query($query, $params = array()) {
+        $curl = new Curl();
+        $params['client_id'] = $this->client_id;
+        $params['access_token'] = $this->access_token;
+        $result = Json::decode($curl->call(self::INSTAGRAM_API_URL . trim($query, '/') . '?' . http_build_query($params), array(), 'GET', true));
+        if($result['meta']['code'] == 200) {
+            return $result['data'];
+        }
+        return $this->debug_mode ? $result : null;
+    }
+
     /**
      * Retourne les dernières photos contenant le tag indiqué
-     * @param string $hashTag   tag (avec ou sans #)
-     * @param integer   $count  Nombre de posts à retourner
-     * @return array Tableau de tableaux associatifs contenant les données des photos
+     * @param string  $hashtag      Tag (avec ou sans #)
+     * @param integer $count        Nombre de posts à retourner
+     * @return array                Tableau de tableaux associatifs contenant les données des photos
      */
-    public function getLastPicturesContainingHashtag($hashTag, $count = 100){
-        $hashTag = StringTools::replaceAll($hashTag, '#', '');
-        
-        $curl = new Curl();
-        $r = $curl->call(self::INSTAGRAM_API_URL.'tags/'.$hashTag.'/media/recent?client_id='.$this->clientId.'&count='.$count, array(), 'GET', true);
-        
-        return Json::decode($r);
+    public function getLastPicturesContainingHashtag($hashtag, $count = 100){
+        return $this->query('tags/'.trim($hashtag, '#').'/media/recent', array(
+            'count' => $count
+        ));
     }
+
+    /**
+     * Retourne les dernières photos contenant le tag indiqué
+     * @param string  $user_id      Tag (avec ou sans #)
+     * @param integer $count        Nombre de posts à retourner
+     * @return array                Tableau de tableaux associatifs contenant les données des photos
+     */
+    public function getLastPicturesFromUser($user_id, $count = 100){
+        return $this->query('users/'.$user_id.'/media/recent', array(
+            'count' => $count
+        ));
+    }
+
 }
