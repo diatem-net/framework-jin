c3ac5bd34a01a06f5067e6f3c1b361d675e34923
Samuel Marchal <smarchal@diatem.net>
2015-12-15 15:13:04 +0100

    Improve approximate queries

--- jin/external/diatem/sherlock/searchcriterias/approximateontext.php
+++ jin/external/diatem/sherlock/searchcriterias/approximateontext.php
@@ -10,9 +10,9 @@ use jin\external\diatem\sherlock\SearchItemInterface;
 
 /** Filtre Sherlock de type critère : recherche approximative sur un ou plusieurs termes
  *
- * 	@auteur		Loïc Gerard
- * 	@version	0.0.1
- * 	@check		
+ *  @auteur     Loïc Gerard
+ *  @version    0.0.1
+ *  @check
  */
 class ApproximateOnText implements SearchItemInterface{
     /**
@@ -20,37 +20,36 @@ class ApproximateOnText implements SearchItemInterface{
      * @var array Noms des champs sur lesquels appliquer le filtre
      */
     private $fields;
-    
+
     /**
      *
      * @var string  Valeur de test
      */
     private $values;
-    
-    
-    /**	Constructeur
-     * 
+
+
+    /** Constructeur
+     *
      * @param array $fields Noms des champs sur lesquels appliquer le filtre
      * @param string $values  Valeur de test
      */
     public function __construct($fields, $values) {
-	$this->fields = $fields;
-	$this->values = $values;
+        $this->fields = $fields;
+        $this->values = $values;
     }
-    
-    
-    /**	Construit le tableau destiné à être ajouté dans une requête de recherche par SherlockSearch
-     * 
-     * @return array	Paramètres de recherche SherlockSearch
+
+
+    /** Construit le tableau destiné à être ajouté dans une requête de recherche par SherlockSearch
+     *
+     * @return array    Paramètres de recherche SherlockSearch
      */
-    public function getParamArray(){
-	
-	$critArray = array();
-	$critArray['fuzzy_like_this'] = array();
-	$critArray['fuzzy_like_this']['fields'] = $this->fields;
-	$critArray['fuzzy_like_this']['like_text'] = $this->values;
-	$critArray['fuzzy_like_this']['max_query_terms'] = 60;
-	
-	return array($critArray);
+    public function getParamArray() {
+        $critArray = array();
+        $critArray['fuzzy_like_this'] = array();
+        $critArray['fuzzy_like_this']['fields'] = $this->fields;
+        $critArray['fuzzy_like_this']['like_text'] = $this->values;
+        $critArray['fuzzy_like_this']['max_query_terms'] = 60;
+
+        return array($critArray);
     }
 }
