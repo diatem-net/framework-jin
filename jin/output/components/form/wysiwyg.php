1c6a8e5e2cae04ecf215564877a4ed3c042ff777
Loic Gerard <lgerard@diatem.net>
2016-05-27 15:03:34 +0200

    Fix. quand plusieurs composants Wysiwyg sur la mm page

--- jin/output/components/form/wysiwyg.php
+++ jin/output/components/form/wysiwyg.php
@@ -1,8 +1,10 @@
 <?php
+
 /**
  * Jin Framework
  * Diatem
  */
+
 namespace jin\output\components\form;
 
 use jin\output\components\form\FormComponent;
@@ -15,42 +17,39 @@ use jin\filesystem\AssetFile;
  * 	@version	0.0.1
  * 	@check		
  */
-class Wysiwyg extends FormComponent implements ComponentInterface{
-    
-    
-    public static $isJsLoaded = FALSE;
-    
-    /**
-     * Constructeur
-     * @param string $name  Nom du composant
-     */
-    public function __construct($name) {
-	parent::__construct($name, 'wysiwyg');
-    }
-     
-    
-    
-    
-    /**
-     * Rendu du composant
-     * @return type
-     */
-    public function render(){
-        
-        if(!self::$isJsLoaded){
-            $html = self::loadWysiwygJs(); 
-        }               
-        
-	$html .= parent::render();	
-	return $html;
-    }
-    
-    public static function loadWysiwygJs(){
-        self::$isJsLoaded = TRUE;
-        $af = new AssetFile('wysiwyg/js.tpl');        
-        return $af->getContent();
-    }
-    
-    
-    
+class Wysiwyg extends FormComponent implements ComponentInterface {
+
+	public static $isJsLoaded = FALSE;
+
+	/**
+	 * Constructeur
+	 * @param string $name  Nom du composant
+	 */
+	public function __construct($name) {
+		parent::__construct($name, 'wysiwyg');
+	}
+
+	/**
+	 * Rendu du composant
+	 * @return type
+	 */
+	public function render() {
+
+		if (!self::$isJsLoaded) {
+			$html = self::loadWysiwygJs();
+			$html .= parent::render();
+		} else {
+			$html = parent::render();
+		}
+
+
+		return $html;
+	}
+
+	public static function loadWysiwygJs() {
+		self::$isJsLoaded = TRUE;
+		$af = new AssetFile('wysiwyg/js.tpl');
+		return $af->getContent();
+	}
+
 }
