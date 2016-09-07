460c3c4cd8ebadd173c2c88d7af4ff803d0ee282
Loïc Gerard <lgerard@diatem.net>
2016-09-07 09:45:16 +0200

    Fix. Imap ext. lib pour gestion UTF8

--- jin/_extlibs/imap-client/Imap.php
+++ jin/_extlibs/imap-client/Imap.php
@@ -758,7 +758,10 @@ class Imap {
 		if ($encoding == "UTF-8") {
 			$text = mb_convert_encoding($text, 'UTF-8', 'UTF-8');
 		}
-		$out = iconv(mb_detect_encoding($text, mb_detect_order(), false), "UTF-8//IGNORE", $text);
+		if (!$encoding) {
+			return utf8_encode($text);
+		}
+		$out = iconv(mb_detect_encoding($text, mb_detect_order(), true), "UTF-8//IGNORE", $text);
 		return $out;
 	}
 
